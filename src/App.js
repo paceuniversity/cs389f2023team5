1d693f3163f4946524d57d966fbe67db1b3c8daf
Aqwues <98438183+Aqwues@users.noreply.github.com>
2023-11-27 04:16:32 -0500

    edits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 
+import Exercise from "./exerciseInfo/Exercise";
 import CustomizeTabs from "./customize/CustomizeTabs";
 
 //import './App.css';
 
 function App() {
   return (
-    <div className="regiment-app">
-      <CustomizeTabs />
-    </div>
+    <div className="exercise-app">
+      <Exercise />
+      </div>
   );
 }
 
